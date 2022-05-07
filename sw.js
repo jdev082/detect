c8b6a57234dfc2f01a3a37603c1e995abd644f00
JaydenDev <willsjayden2@gmail.com>
2022-05-07 13:46:51 -0500

    Attempts

--- sw.js
+++ sw.js
@@ -1,10 +1,3 @@
-self.addEventListener('install', (e) => {
-    console.log('[Service Worker] Install');
-  });
-
-self.addEventListener('activate', (e) => {
-    console.log('[Service Worker] Activate');
-});
 
 const ASSETS = [
     "/js/global.js",
@@ -14,44 +7,25 @@ const ASSETS = [
     "/css/style.css",
     "manifest.json",
     "sw.js"
-];
+]; 
 
-self.addEventListener('fetch', (e) => {
-    console.log('[Service Worker] Fetching');
-    e.respondWith(
-        caches.match(e.request)
-        .then(res => {
-            if(res) {
-                return res;
-            }
-            return fetch(e.request);
-        }
-        )
-    );
-    });
-
-    addEventListener('fetch', function(event) {
-        event.respondWith(
-          caches.match(event.request)
-            .then(function(response) {
-              if (response) {
-                return response;
-              } else {
-                return fetch(event.request)
-                  .then(function(res) {
-                    return caches.open(CACHE_DYNAMIC_NAME)
-                      .then(function(cache) {
-                        cache.put(event.request.url, res.clone());
-                        return res;
-                      })
-                  })
-                  .catch(function(err) {
-                    return caches.open(CACHE_CONTAINING_ERROR_MESSAGES)
-                      .then(function(cache) {
-                        return cache.match('/offline.html');
-                      });
-                  });
-              }
+let cache_name = "cacheName";
+self.addEventListener("install", event => {
+    console.log("installing...");
+    event.waitUntil(
+        caches
+            .open(cache_name)
+            .then(cache => {
+                return cache.addAll(assets);
             })
-        );
-      });     
+            .catch(err => console.log(err))
+    );
+});
+
+self.addEventListener('install', (e) => {
+    console.log('[Service Worker] Install');
+  });
+
+self.addEventListener('activate', (e) => {
+    console.log('[Service Worker] Activate');
+});
