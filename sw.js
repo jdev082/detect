3744337ad5965a0ec51fe6d1272faa57fec79f37
JaydenDev <willsjayden2@gmail.com>
2022-05-07 13:36:35 -0500

    maybe someday it'll work!

--- sw.js
+++ sw.js
@@ -11,12 +11,35 @@ const ASSETS = [
     "/",
     "/index.html",
     "favicon.png",
-    "/css/style.css"
+    "/css/style.css",
+    "manifest.json",
+    "sw.js"
 ];
 
 self.addEventListener('fetch', (e) => {
-    if (ASSETS.includes(e.request.url)) {
-        e.respondWith(fetch(e.request));
-    }
-}
-);
+    console.log('[Service Worker] Fetching');
+    e.respondWith(
+        caches.match(e.request)
+        .then(res => {
+            if(res) {
+                return res;
+            }
+            return fetch(e.request);
+        }
+        )
+    );
+    });
+
+    self.addEventListener('fetch', (e) => {
+        console.log('[Service Worker] Fetching');
+        e.respondWith(
+            caches.match(e.request)
+            .then(res => {
+                if(res) {
+                    return res;
+                }
+                return fetch(e.request);
+            }
+            )
+        );
+    });
