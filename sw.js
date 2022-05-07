90031f26ccfb6c7192f5ea034a7b880978df4f43
JaydenDev <willsjayden2@gmail.com>
2022-05-07 14:04:09 -0500

    chances...

--- sw.js
+++ sw.js
@@ -1,12 +1,3 @@
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('sw.js', {
-        scope: '.'
-    }).then(function(registration) {
-        console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }, function(err) {
-        console.log('ServiceWorker registration failed: ', err);
-    });
-}
 
 const ASSETS = [
     "/js/global.js",
