2cd63a80a2abefb7ecd4f20da949f91f614e53df
JaydenDev <willsjayden2@gmail.com>
2022-05-07 13:18:04 -0500

    Making it acccessible offline 1

--- sw.js
+++ sw.js
@@ -5,3 +5,27 @@ self.addEventListener('install', (e) => {
 self.addEventListener('activate', (e) => {
     console.log('[Service Worker] Activate');
 });
+
+const ASSETS = [
+    "/js/global.js",
+    "/",
+    "/index.html",
+    "favicon.png",
+    "/css/style.css"
+];
+
+self.addEventListener("fetch", event => {
+    if (event.request.url === "https://jdev.eu.org/deteect") {
+        event.respondWith(
+            fetch(event.request).catch(err =>
+                self.cache.open(cache_name).then(cache => cache.match("/offline.html"))
+            )
+        );
+    } else {
+        event.respondWith(
+            fetch(event.request).catch(err =>
+                caches.match(event.request).then(response => response)
+            )
+        );
+    }
+});
