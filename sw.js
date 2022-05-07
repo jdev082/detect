aeff91c4612d12b897f8fb8a15120fe3c20b38a9
JaydenDev <willsjayden2@gmail.com>
2022-05-07 16:09:33 -0500

    Maybe?

--- sw.js
+++ sw.js
@@ -1,68 +1,30 @@
-
-const ASSETS = [
-    "/js/global.js",
-    "/index.html",
-    "favicon.png",
-    "/css/style.css",
-    "manifest.json",
-]; 
-
-let CACHE_NAME = "cacheName";
-self.addEventListener("install", event => {
-    console.log("installing...");
-    event.waitUntil(
-        caches
-            .open(cache_name)
-            .then(cache => {
-                return cache.addAll(ASSETS);
-            })
-            .catch(err => console.log(err))
-    );
+self.addEventListener('install', (e) => {
+  console.log('[Service Worker] Install');
+  e.waitUntil((async () => {
+    const cache = await caches.open(cacheName);
+    console.log('[Service Worker] Caching all: app shell and content');
+    await cache.addAll(contentToCache);
+  })());
 });
 
-/* Source https://packager.turbowarp.org/sw.js, TurboWarp project */
-self.addEventListener('install', event => {
-  self.skipWaiting();
-  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS.map(i => i === '' ? base : i))));
-});
+const cacheName = 'cache';
+const appShellFiles = [
+  '/',
+  '/index.html',
+  '/js/global.js',
+  'favicon.png',
+  'css/style.css'
+];
 
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys => Promise.all(keys.filter(i => i !== CACHE_NAME).map(i => caches.delete(i))))
-  );
+self.addEventListener('fetch', (e) => {
+  e.respondWith((async () => {
+    const r = await caches.match(e.request);
+    console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+    if (r) { return r; }
+    const response = await fetch(e.request);
+    const cache = await caches.open(cacheName);
+    console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+    cache.put(e.request, response.clone());
+    return response;
+  })());
 });
-
-const fetchWithTimeout = (req) => new Promise((resolve, reject) => {
-    const timeout = setTimeout(reject, 5000);
-    fetch(req)
-      .then((res) => {
-        clearTimeout(timeout);
-        resolve(res);
-      })
-      .catch((err) => {
-        clearTimeout(timeout);
-        reject(err);
-      });
-  });
-  
-  self.addEventListener('fetch', event => {
-    if (event.request.method !== 'GET') return;
-    const url = new URL(event.request.url);
-    if (url.origin !== location.origin) return;
-    const relativePathname = url.pathname.substr(base.length);
-    if (IS_PRODUCTION && ASSETS.includes(relativePathname)) {
-      url.search = '';
-      const immutable = !!relativePathname;
-      if (immutable) {
-        event.respondWith(
-          caches.match(new Request(url)).then((res) => res || fetch(event.request))
-        );
-      } else {
-        event.respondWith(
-          fetchWithTimeout(event.request).catch(() => caches.match(new Request(url)))
-        );
-      }
-    }
-  });
-  
-/* End of borrowed source */
