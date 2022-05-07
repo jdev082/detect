2b92f504b7f57bd8bb4866341d646d0e78413e29
JaydenDev <willsjayden2@gmail.com>
2022-05-07 13:42:25 -0500

    maybe someday it'll work!

--- sw.js
+++ sw.js
@@ -30,16 +30,28 @@ self.addEventListener('fetch', (e) => {
     );
     });
 
-    self.addEventListener('fetch', (e) => {
-        console.log('[Service Worker] Fetching');
-        e.respondWith(
-            caches.match(e.request)
-            .then(res => {
-                if(res) {
-                    return res;
-                }
-                return fetch(e.request);
-            }
-            )
+    addEventListener('fetch', function(event) {
+        event.respondWith(
+          caches.match(event.request)
+            .then(function(response) {
+              if (response) {
+                return response;
+              } else {
+                return fetch(event.request)
+                  .then(function(res) {
+                    return caches.open(CACHE_DYNAMIC_NAME)
+                      .then(function(cache) {
+                        cache.put(event.request.url, res.clone());
+                        return res;
+                      })
+                  })
+                  .catch(function(err) {
+                    return caches.open(CACHE_CONTAINING_ERROR_MESSAGES)
+                      .then(function(cache) {
+                        return cache.match('/offline.html');
+                      });
+                  });
+              }
+            })
         );
-    });
+      });     
